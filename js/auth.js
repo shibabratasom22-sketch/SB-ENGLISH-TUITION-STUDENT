af3eda292bb8fe57768736f961150b1c36f06e6b
shibabratasom22-sketch <shibabratasom22@gmail.com>
2026-07-27 18:49:46 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,54 @@
+// ===============================
+// SB ENGLISH TUITION LOGIN
+// ===============================
 
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", loginUser);
+
+function loginUser() {
+
+    const studentId = document.getElementById("studentId").value.trim().toUpperCase();
+    const password = document.getElementById("password").value;
+    const msg = document.getElementById("loginMsg");
+
+    msg.innerHTML = "";
+
+    if(studentId === "" || password === ""){
+        msg.innerHTML = "Enter Student ID & Password";
+        return;
+    }
+
+    let email;
+
+    // Admin Login
+    if(studentId === "ADMIN"){
+        email = "admin@sbenglishtuition.app";
+    }
+
+    // Student Login
+    else{
+        email = studentId.toLowerCase() + "@sbenglishtuition.app";
+    }
+
+    auth.signInWithEmailAndPassword(email,password)
+
+    .then(()=>{
+
+        msg.style.color="green";
+        msg.innerHTML="Login Successful...";
+
+        setTimeout(()=>{
+            window.location.href="dashboard.html";
+        },1000);
+
+    })
+
+    .catch((error)=>{
+
+        msg.style.color="red";
+        msg.innerHTML="Invalid ID or Password";
+
+    });
+
+}
