bd7dbd65182a0141ba2c2070112805434c0006b0
shibabratasom22-sketch <shibabratasom22@gmail.com>
2026-07-27 23:16:50 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// ===============================
+// SB ENGLISH TUITION
+// app.js
+// ===============================
 
+console.log("SB English Tuition App Loaded");
+
+// ভবিষ্যতে এখানেই
+// - Dashboard
+// - Student Profile
+// - Books
+// - Homework
+// - Notice
+// - Fees
+// - Results
+// এর সাধারণ কোড থাকবে।
