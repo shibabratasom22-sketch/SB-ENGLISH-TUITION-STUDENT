38f50039408007e10a1715379b7ed430af4b664f
shibabratasom22-sketch <shibabratasom22@gmail.com>
2026-07-28 18:59:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-// Service Worker
+const CACHE_NAME = "sb-english-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./students.html",
+  "./admin.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/auth.js",
+  "./js/students.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
